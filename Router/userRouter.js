898839ba8a647816528aa7bc42ddd06575a28988
dlafhr789 <dlafhr789@naver.com>
2024-07-09 02:21:26 +0900

    user swagger 추가

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -5,28 +5,49 @@ const userController = require('../Service/userService.js');
 
 /**
  * @swagger
- *  /user/checkDuplicatedId/{id}:
- *    get:
- *      tags: [유저]
- *      summary: 아이디 중복확인 API
- *      produces:
- *      - application/json
- *      parameters:
- *        - name: id
- *          in: path
- *          required: true
- *          schema:
- *            type: string
- *          description: "중복확인할 아이디"
- *      responses:
- *        200:
- *          description: 로그인 성공
- *        401:
- *          description: 비밀번호 불일치
- *        404:
- *          description: 존재하지 않는 아이디
- *        500:
- *          description: 로그인 실패
+ * /user/checkDuplicatedId/{id}:
+ *   get:
+ *     tags: [유저]
+ *     summary: 아이디 중복 확인 API
+ *     description: 입력한 아이디가 이미 사용 중인지 확인하는 API입니다.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: 사용할 아이디
+ *     responses:
+ *       200:
+ *         description: 사용 가능한 아이디
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 사용 가능한 아이디 입니다.
+ *       409:
+ *         description: 중복된 아이디
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 중복된 아이디입니다.
+ *       500:
+ *         description: 서버 오류
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 아이디 중복확인 중 오류 발생
  */
 router.get('/checkDuplicatedId/:id', userController.checkDuplicatedId);
 
@@ -98,198 +119,277 @@ router.get('/getUserData/:id', userController.getUserData);
 
 /**
  * @swagger
- *  /user/signIn:
- *    post:
- *      tags: [유저]
- *      summary: 로그인 API
- *      produces:
- *      - application/json
- *      parameters:
- *        - in: "body"
- *          name: "body"
- *          description: "로그인 정보 입력"
- *          required: true
- *          schema:
- *            type: object
- *            properties:
- *              data:
- *                type: object
- *                properties:
- *                  id:
- *                      type: string
- *                      example: 'testid'
- *                  password:
- *                      type: string
- *                      example: 'testpassword'
- *      responses:
- *        200:
- *          description: 로그인 성공
- *        401:
- *          description: 비밀번호 불일치
- *        404:
- *          description: 존재하지 않는 아이디
- *        500:
- *          description: 로그인 실패
+ * /user/signIn:
+ *   post:
+ *     summary: 로그인 API
+ *     description: 사용자 로그인을 처리하는 API입니다.
+ *     tags: [유저]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               data:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: string
+ *                     description: 사용자 아이디
+ *                     example: testid2
+ *                   password:
+ *                     type: string
+ *                     format: password
+ *                     description: 사용자 비밀번호
+ *                     example: 123123
+ *     responses:
+ *       200:
+ *         description: 로그인 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 로그인에 성공하였습니다!
+ *                 userId:
+ *                   type: string
+ *                   example: testid
+ *       401:
+ *         description: 비밀번호 불일치
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 비밀번호가 일치하지 않습니다
+ *       404:
+ *         description: 존재하지 않는 아이디
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 존재하지 않는 아이디입니다.
+ *       500:
+ *         description: 서버 오류
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 로그인 중 오류 발생
  */
 router.post('/signIn', userController.signIn);
 
 /**
  * @swagger
- *  /user/signUp:
- *    post:
- *      tags: [유저]
- *      summary: 회원가입 API
- *      produces:
- *      - application/json
- *      parameters:
- *        - in: "body"
- *          name: "body"
- *          description: "회원가입 정보 입력"
- *          required: true
- *          schema:
- *              type: object
- *              properties:
- *                data:
- *                  type: object
- *                  properties:
- *                      id:
- *                        type: string
- *                        example: 'testid'
- *                      password:
- *                        type: string
- *                        example: 'testpassword'
- *                      email:
- *                        type: string
- *                        example: 'test@test.com'
- *                      phone:
- *                        type: string
- *                        example: '010-1234-5678'
- *                      name:
- *                        type: string
- *                        example: '김테스트'
- *                      image:
- *                        type: string
- *                        example: '아직 구현중.. 되면 업데이트함'
- *      responses:
- *        201:
- *          description: 회원가입 성공
- *        409:
- *          description: 이미 사용중인 아이디
- *        500:
- *          description: 회원가입 중 오류
+ * /user/signUp:
+ *   post:
+ *     tags: [유저]
+ *     summary: 회원가입 API
+ *     description: 사용자가 회원가입을 요청하는 API입니다.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               data:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: string
+ *                     description: 사용할 아이디
+ *                     example: testid
+ *                   password:
+ *                     type: string
+ *                     format: password
+ *                     description: 사용할 비밀번호
+ *                     example: testpassword
+ *                   email:
+ *                     type: string
+ *                     format: email
+ *                     description: 이메일 주소
+ *                   phone:
+ *                     type: string
+ *                     description: 전화번호
+ *                     example: 010-1234-1234
+ *                   name:
+ *                     type: string
+ *                     description: 사용자 이름
+ *                     example: 김수한무거북이와두루미
+ *     responses:
+ *       201:
+ *         description: 회원가입 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 회원가입 성공
+ *       409:
+ *         description: 이미 사용 중인 아이디
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 이미 사용중인 아이디입니다.
+ *       500:
+ *         description: 서버 오류
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 회원가입 중 오류 발생
  */
 router.post('/signUp', userController.signUp);
 
 /**
  * @swagger
- *  /user/editUser:
- *    post:
- *      tags: [유저]
- *      summary: 유저 정보 수정 API
- *      produces:
- *      - application/json
- *      parameters:
- *        - in: "body"
- *          name: "body"
- *          description: "수정된 유저 정보 입력"
- *          required: true
- *          schema:
- *              type: object
- *              properties:
- *                data:
- *                  type: object
- *                  properties:
- *                      id:
- *                        type: string
- *                        example: 'testid'
- *                      password:
- *                        type: string
- *                        example: 'testpassword'
- *                      email:
- *                        type: string
- *                        example: 'test@test.com'
- *                      phone:
- *                        type: string
- *                        example: '010-1234-5678'
- *                      name:
- *                        type: string
- *                        example: '김테스트'
- *                      image:
- *                        type: string
- *                        example: '아직 구현중.. 되면 업데이트함'
- *      responses:
- *        200:
- *          description: 회원가입 성공
- *        404:
- *          description: 존재하지 않는 아이디
- *        500:
- *          description: 회원가입 중 오류
+ * user/editUser:
+ *   post:
+ *     summary: 유저 정보 수정 API
+ *     description: 사용자 정보를 수정하는 API입니다. 이미지를 제외한 모든 값들이 필수로 입력되어야 합니다.<br>모든 값들 채워서 보내야 해요. 비운 값 있으면 그대로 null로 들어가기 때문!!<br>+ 이미지같은 경우 주소 빼고 이미지 이름만 넣기 ex) asdff3-dfada.png
+ *     tags: [유저]
+ *     consumes:
+ *       - multipart/form-data
+ *     parameters:
+ *       - in: formData
+ *         name: data
+ *         required: true
+ *         type: string
+ *         description: "수정할 사용자 정보(JSON 형태로 전달). 포함 필드: id, password, email, phone, name."
+ *       - in: formData
+ *         name: image
+ *         type: file
+ *         description: 사용자 프로필 이미지 파일 (선택 사항).
+ *     responses:
+ *       200:
+ *         description: 유저 정보 수정 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 유저 정보 수정 성공
+ *       404:
+ *         description: 존재하지 않는 아이디
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 존재하지 않는 아이디
+ *       500:
+ *         description: 서버 오류
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 유저 정보 수정 중 오류
  */
 router.post('/editUser', userController.editUser);
 
 /**
  * @swagger
- *  /user/findId:
- *    post:
- *      tags: [유저]
- *      summary: 아이디 찾기 API
- *      produces:
- *      - application/json
- *      parameters:
- *        - in: "body"
- *          name: "body"
- *          description: "수정된 유저 정보 입력"
- *          required: true
- *          schema:
- *              type: object
- *              properties:
- *                data:
- *                  type: object
- *                  properties:
- *                      name:
- *                        type: string
- *                        example: 홍길동
- *                      email:
- *                        type: string
- *                        example: test@test.com
- *                      phone:
- *                        type: string
- *                        example: 010-1234-1234
- *      responses:
- *        200:
- *          description: 아이디 찾기 성공
- *          content:
- *            application/json:
- *              schema:
- *                type: object
- *                properties:
- *                  message:
- *                    type: string
- *                    example: 아이디 찾기 성공
- *                  data:
- *                    type: array
- *                    items:
- *                      type: object
- *                      properties:
- *                        id:
- *                          type: string
- *                          example: 123
- *              examples:
- *                example1:
- *                  value:
- *                    message: 아이디 찾기 성공
- *                    data:
- *                     - id: thisisid
- *                example2:
- *                  value:
- *                    message: 아이디 찾기 성공
- *                    data:
- *                     - id: testid1
- *                     - id: testid2
- *        404:
- *          description: 일치하는 계정 없음
- *        500:
- *          description: 아이디 찾기 중 오류
+ * user/findId:
+ *   post:
+ *     summary: 유저 아이디 찾기 API
+ *     description: |
+ *       이름, 이메일, 전화번호를 기준으로 유저 아이디를 찾는 API입니다.
+ *       
+ *       찾고자 하는 유저의 이름, 이메일, 전화번호를 요청 본문에 JSON 형태로 전달합니다.
+ *       
+ *       만약 일치하는 유저가 없을 경우 404 상태 코드와 함께 '일치하는 유저가 없음' 메시지가 반환됩니다.
+ *       
+ *       성공 시 200 상태 코드와 함께 '아이디 찾기 성공' 메시지와 찾은 유저의 아이디 목록이 반환됩니다.
+ *       
+ *     tags: [유저]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: 유저의 이름
+ *               email:
+ *                 type: string
+ *                 description: 유저의 이메일
+ *               phone:
+ *                 type: string
+ *                 description: 유저의 전화번호
+ *             example:
+ *               name: 홍길동
+ *               email: example@example.com
+ *               phone: "010-5597-1123"
+ *     responses:
+ *       200:
+ *         description: 아이디 찾기 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 아이디 찾기 성공
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         description: 유저 아이디
+ *                         example: testid
+ *       404:
+ *         description: 일치하는 유저가 없음
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 일치하는 유저가 없음
+ *       500:
+ *         description: 서버 오류
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 아이디 찾기 중 오류
  */
 router.post('/findId', userController.findId);
 
