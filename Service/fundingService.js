8155e02b314641675739abbd1cc3dde9a71457b2
yejin-e <yejin01e@gmail.com>
2024-07-08 12:37:51 +0000

    펀딩 참여

--- Service/fundingService.js
+++ Service/fundingService.js
@@ -4,8 +4,15 @@ const funding = require('../Model/funding');
 // 펀딩 생성
 async function createFunding(req, res) {
 	try {
-		const { title, item, price, deadline, image, userId } = req.body;
-
+		const { title, item, price, deadline, image, userId } = req.body.data;
+		console.log('Received request body:', req.body);
+		// Check if all required fields are present
+		if (!title || !item || !price || !deadline || !userId) {
+			return res.status(400).json({ 
+				error: 'title:', title
+			});
+		}
+		
 		const newFunding = await funding.create({
 			title,
 			item,
@@ -20,13 +27,13 @@ async function createFunding(req, res) {
 		res.status(201).json({ message: '펀딩 생성' });
 	} catch (err) {
 		console.error('펀딩 생성 실패:', err);
-		res.status(500).json({ error: '펀딩 생성 실패' });
+		res.status(500).json({ error: '펀딩 생성 실패', err });
 	}
 }
 
 // 펀딩 리스트 조회
 async function viewListFunding(req, res) {
-	const { userId } = req.body; // 요청 바디에서 userId 추출
+	const { userId } = req.body.data; // 요청 바디에서 userId 추출
 
 	try {
 		// 요청 바디에서 userId가 없는 경우 처리
@@ -84,7 +91,7 @@ async function viewFunding(req, res) {
 // 특정 펀딩 수정
 async function updateFunding(req, res) {
 	try {
-		const { id, title, item, price, deadline, image } = req.body;
+		const { id, title, item, price, deadline, image } = req.body.data;
 
 		// 요청 바디에서 id가 없는 경우 처리
 		if (!id) {
@@ -119,7 +126,7 @@ async function updateFunding(req, res) {
 
 // 특정 펀딩 삭제
 async function deleteFunding(req, res) {
-	const { id } = req.body; // 요청 바디에서 id 추출
+	const { id } = req.body.data; // 요청 바디에서 id 추출
 
 	try {
 		// 요청 바디에서 id가 없는 경우 처리
@@ -147,8 +154,46 @@ async function deleteFunding(req, res) {
 	}
 }
 
+// 펀딩 참여
+async function participateFunding(req, res) {
+	try {
+		const { id, price } = req.body.data;
+
+		// 요청 바디에서 id가 없는 경우 처리
+		if (!id) {
+			return res.status(400).json({ error: 'request body에 id가 없습니다.' });
+		}
+		
+		// 요청 바디에서 id가 없는 경우 처리
+		if (!price) {
+			return res.status(400).json({ error: 'request body에 price가 없습니다.' });
+		}
+		
+		const newFunding = await funding.findOne({
+			where: { id: id },
+		});
+
+		if (!newFunding) {
+			console.log('펀딩 미존재');
+			return res.status(404).json({ error: '존재하지 않는 펀딩입니다.' });
+		}
+
+		// 펀딩 정보 업데이트
+		newFunding.price += price;
+
+		await newFunding.save();
+
+		console.log('펀딩 참여');
+		res.status(201).json({ message: '펀딩 참여' });
+	} catch (err) {
+		console.error('펀딩 참여 실패:', err);
+		res.status(500).json({ error: '펀딩 참여 실패' });
+	}
+}
+
 exports.createFunding = (req, res, next) => createFunding(req, res);
 exports.viewListFunding = (req, res, next) => viewListFunding(req, res);
 exports.viewFunding = (req, res, next) => viewFunding(req, res);
 exports.updateFunding = (req, res, next) => updateFunding(req, res);
 exports.deleteFunding = (req, res, next) => deleteFunding(req, res);
+exports.participateFunding = (req, res, next) => participateFunding(req, res);
